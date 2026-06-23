002cf7e49e61cde176961441356d8d5e6639bdd4
Zefiryn <zefiryn@jewula.net>
2026-06-24 01:36:00 +0200

    Use constant to determine number of decimal places kept in reading value

--- src/Entity/Reading.php
+++ src/Entity/Reading.php
@@ -11,6 +11,8 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Entity(repositoryClass: ReadingRepository::class)]
 class Reading
 {
+    const int DECIMAL_DIVISION = 10;
+
     #[ORM\Id]
     #[ORM\GeneratedValue(strategy: 'SEQUENCE')]
     #[ORM\Column]
